6542ba1aba512c4760ec0b048e77ca729ed494d6
Paul Velasco <42251572+pooljpv84@users.noreply.github.com>
2022-10-29 13:00:17 -0500

    Update Graduacion.java

--- observer2/Graduacion.java
+++ observer2/Graduacion.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package p61.observer2;
 
-/**
- *
- * @author Mauricio Ortiz Ochoa
- */
 public class Graduacion extends Sujeto {
     
     private boolean facturaPagada;
