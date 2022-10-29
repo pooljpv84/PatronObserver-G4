7a5c63e339e73c77eaddf02ddc9277f97ccc6e88
Paul Velasco <42251572+pooljpv84@users.noreply.github.com>
2022-10-29 13:00:36 -0500

    Update Materia.java

--- observer2/Materia.java
+++ observer2/Materia.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package p61.observer2;
 
-/**
- *
- * @author Mauricio Ortiz Ochoa
- */
 public class Materia implements SolicitarGraduacion {
 
     public boolean isSeguimientoIniciado() {
