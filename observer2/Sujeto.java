94b0b63aebb796962389c72d98bcd1fe2842c216
Paul Velasco <42251572+pooljpv84@users.noreply.github.com>
2022-10-29 13:01:02 -0500

    Update Sujeto.java

--- observer2/Sujeto.java
+++ observer2/Sujeto.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package p61.observer2;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author Mauricio Ortiz Ochoa
- */
 public abstract class Sujeto {
     private List<SolicitarGraduacion> pagoGraduacionList = new ArrayList<SolicitarGraduacion>();
     
