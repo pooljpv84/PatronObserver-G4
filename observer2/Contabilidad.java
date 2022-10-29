ce78353450054dbbe8525ba7e88b6d96f663334a
Paul Velasco <42251572+pooljpv84@users.noreply.github.com>
2022-10-29 12:59:58 -0500

    Update Contabilidad.java

--- observer2/Contabilidad.java
+++ observer2/Contabilidad.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package p61.observer2;
 
-/**
- *
- * @author Mauricio Ortiz Ochoa
- */
 public class Contabilidad implements SolicitarGraduacion {
 
     private boolean pagoMayorizado;
